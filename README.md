83d1357b8f2b76d53fb445ec9746cbea7ba87f5a
Christina Martinsson <chmar@bibsent.no>
2020-11-12 15:56:12 +0100

    Added functions, edited query to full posts. Edited readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # VB-bilder
+
+Required libraries: 
+
+```bash
+pip install -r requirements.txt
+```
+
+Henter fullkatalogiserte poster fra VB og datterforlag som mangler omslagsbilde fra Ája via SRU. Laster dem opp til \\Sindre\OmslagsbilderTilAja
--- getvbcovers.py
+++ getvbcovers.py
@@ -1,29 +1,36 @@
 import requests
 from lxml import etree
 
-server = 'https://sru.aja.bs.no/mlnb'
-version = '1.1'
-maxrec = '100'
-schema = 'marc21'
-query = "dc.identifier=97882419*%20AND%20bs.has_cover=true"
+vburl = 'https://res.cloudinary.com/forlagshuset/image/upload/q_auto:best/'
+path = '//Sindre/OmslagsbilderTilAja/'
+jpg = '.jpg'
 
-url = server + '?version=' + version + '&operation=searchRetrieve' + '&maximumRecords=' + maxrec
-url = url + '&recordSchema=' + schema + '&query=' + query
+# Creates a url to get SRU
+def get_isbns():
+    server = 'https://sru.aja.bs.no/mlnb'
+    version = '1.1'
+    maxrec = '10'
+    schema = 'marc21'
+    query = "bs.level=full%20AND%20dc.identifier=97882419*%20AND%20bs.has_cover=false"
+    url = server + '?version=' + version + '&operation=searchRetrieve' + '&maximumRecords=' + maxrec
+    url = url + '&recordSchema=' + schema + '&query=' + query
+    r = requests.get(url)
+    tree =  etree.fromstring(r.text)
 
-print(url)
+    # Dictionary for namespaces (prefix : URI)
+    namespaces = {'marc21' : 'info:lc/xmlns/marcxchange-v1'}
+    xpath = '//marc21:datafield[@tag = "020"]/marc21:subfield[@code ="a"]'
+    return tree.xpath(xpath, namespaces = namespaces)
 
-parser = etree.XMLParser(encoding='utf-8')
+def get_images_from_vb(_isbns):
+    for isbn in _isbns:
+        r = requests.get(vburl+str(isbn.text), allow_redirects=True)
+        # print("url: ", r.url)
+        # print('Status r:', r.status_code)
+        if r:
+            upload_images_to_aja(isbn.text, r.content)
+        
+def upload_images_to_aja(_isbn, _content):
+    open(path + str(_isbn)+jpg, 'wb').write(_content)
 
-r = requests.get(url)
-tree = etree.fromstring(r.text)
-
-# Dictionary for namespaces (prefix : URI)
-
-namespaces = {'marc21' : 'info:lc/xmlns/marcxchange-v1'}
-
-xpath = '//marc21:datafield[@tag = "020"]/marc21:subfield[@code ="a"]'
-
-isbns = tree.xpath(xpath, namespaces = namespaces)
-
-for isbn in isbns:
-    print(isbn.text)
+get_images_from_vb(get_isbns())
